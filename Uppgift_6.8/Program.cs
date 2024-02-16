0a07c72516b45faf3280dd0bb02f204b1aa51659
ArvidUnell <arun06001@utb.vaxjo.se>
2024-02-16 10:06:34 +0100

    Add project files.

---
+++ Uppgift_6.8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Uppgift_6_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ÄrPrimt(27));
+            Console.WriteLine(ÄrPrimt(37));
+        }
+        /// <summary>
+        /// Kollar om ett tal är primt
+        /// </summary>
+        /// <param name="tal">Talet som ska kollas</param>
+        /// <returns>Om talet är primt eller ej</returns>
+        static bool ÄrPrimt(int tal)
+        {
+            if (tal == 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= tal / 2; i++)
+            {
+                if (tal % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
